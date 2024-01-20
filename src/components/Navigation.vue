3416f5dd48b321f651864d5a19c8dec61c51eccf
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2024-01-20 17:31:01 +0200

    nav-bar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,26 +2,26 @@
     <header>
         <nav class="container">
             <div class="branding">
-                <router-link class="header" to="/">HolidayBlog</router-link>
+                <router-link class="header" :to="{ name: 'Home' }">HolidayBlog</router-link>
             </div>
             <div class="nav-links">
                 <ul>
-                    <router-link class="link" to="/">Home</router-link>
-                    <router-link class="link" to="/blogs">Blogs</router-link>
-                    <router-link class="link" to="/create-post">Create Post</router-link>
-                    <router-link class="link" to="/login">Login/Register</router-link>
+                    <router-link class="link" to="#">Home</router-link>
+                    <router-link class="link" to="#">Blogs</router-link>
+                    <router-link class="link" to="#">Create Post</router-link>
+                    <router-link class="link" to="#">Login/Register</router-link>
                 </ul>
             </div>
         </nav>
 
-        <img class="menu-icon" :src="menuIcon" alt="Menu Icon" />
+        <img :src="menuIcon" alt="Menu Icon" class="menu-icon" />
 
         <transition name="mobile-nav">
-            <ul>
-                <router-links class="link" to="#">Home</router-links>
-                <router-links class="link" to="#">Blogs</router-links>
-                <router-links class="link" to="#">Create Post</router-links>
-                <router-links class="link" to="#">Login/Register</router-links>
+            <ul class="mobile-nav">
+                <router-link class="link" to="#">Home</router-link>
+                <router-link class="link" to="#">Blogs</router-link>
+                <router-link class="link" to="#">Create Post</router-link>
+                <router-link class="link" to="#">Login/Register</router-link>
             </ul>
         </transition>
     </header>
@@ -31,14 +31,54 @@
 import menuIcon from '../assets/Icons/bars-regular.svg';
 
 export default {
-    name: 'Navigation',
-    components: {},
-    setup() {
-      return {
-        menuIcon
-      };
-    }
+    name: 'navigation',
+    data() {
+        return {
+            menuIcon: menuIcon,
+        };
+    },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    background-color: #fff;
+    padding: 0 25px;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    z-index: 99;
+
+    .link {
+        font-weight: 500;
+        padding: 0 8px;
+        transition: .3s color ease;
+
+        &:hover {
+            color: #1eb8b8;
+        }
+    }
+
+    nav {
+        display: flex;
+        padding: 25px 0;
+
+        .branding {
+            display: flex;
+            align-items: center;
+
+            .header {
+                font-weight: 600;
+                font-size: 24px;
+                color: #000;
+                text-decoration: none;
+            }
+        }
+
+        .nav-links {
+            position: relative;
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+        }
+    }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,33 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue-svg')
+      .test(/\.svg$/)
+      .use('vue-svg-inline-loader')
+      .loader('vue-svg-inline-loader')
+      .end();
+  },
+};
+
+
+// module.exports = {
+//   chainWebpack: (config) => {
+//     const svgRule = config.module.rule("svg");
+
+//     // svgRule.uses.clear();
+
+//     svgRule
+//       .use("babel-loader")
+//       .loader("babel-loader")
+//       .end();
+
+//     svgRule
+//       .use("vue-svg-loader")
+//       .loader("vue-svg-loader");
+//   },
+// };
+
