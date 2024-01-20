08db733e9ddce3eabe3d6d5b9e5ba526c22262dd
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2024-01-20 23:51:26 +0200

    nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,7 +5,7 @@
                 <router-link class="header" :to="{ name: 'Home' }">HolidayBlog</router-link>
             </div>
             <div class="nav-links">
-                <ul>
+                <ul v-show="!mobile">
                     <router-link class="link" to="#">Home</router-link>
                     <router-link class="link" to="#">Blogs</router-link>
                     <router-link class="link" to="#">Create Post</router-link>
@@ -14,10 +14,10 @@
             </div>
         </nav>
 
-        <img :src="menuIcon" alt="Menu Icon" class="menu-icon" />
+        <img :src="menuIcon" alt="Menu Icon" class="menu-icon" @click="toggleMobileNav" v-show="mobile" />
 
         <transition name="mobile-nav">
-            <ul class="mobile-nav">
+            <ul class="mobile-nav" v-show="mobileNav">
                 <router-link class="link" to="#">Home</router-link>
                 <router-link class="link" to="#">Blogs</router-link>
                 <router-link class="link" to="#">Create Post</router-link>
@@ -35,7 +35,31 @@ export default {
     data() {
         return {
             menuIcon: menuIcon,
+
+            mobile: null,
+            mobileNav: null,
+            windowWidth: null
         };
+    },
+    created() {
+        window.addEventListener('resize', this.checkScreen);
+        this.checkScreen();
+    },
+    methods: {
+        checkScreen() {
+            this.windowWidth = window.innerWidth;
+            if (this.windowWidth <= 750) {
+                this.mobile = true;
+                return;
+            }
+            this.mobile = false;
+            this.mobileNav = false;
+            return;
+        },
+
+        toggleMobileNav() {
+            this.mobileNav = !this.mobileNav;
+        }
     },
 };
 </script>
@@ -76,9 +100,70 @@ header {
         .nav-links {
             position: relative;
             display: flex;
+            flex: 1;
             align-items: center;
             justify-content: flex-end;
+
+            ul {
+                margin-right: 32px;
+
+                .link {
+                    margin-right: 32px;
+                }
+
+                .link:last-child {
+                    margin-right: 0;
+                }
+            }
         }
     }
+
+    .menu-icon {
+        cursor: pointer;
+        position: absolute;
+        top: 32px;
+        right: 25px;
+        height: 25px;
+        width: auto;
+    }
+
+    .mobile-nav {
+        padding: 20px;
+        width: 70%;
+        max-width: 250px;
+        display: flex;
+        flex-direction: column;
+        position: fixed;
+        height: 100%;
+        background-color: #303030;
+        top: 0;
+        left: 0;
+
+        .link {
+            padding: 15px 0;
+            color: #fff;
+        }
+    }
+
+    .mobile-nav-enter-active {
+        transition: all 1s ease;
+    }
+    
+    .mobile-nav-leave-active {
+        transition: all 1s ease;
+    }   
+
+    .mobile-nav-enter {
+        transform: translateX(-250px);
+    }
+
+    .mobile-nav-enter-to {
+        transform: translateX(0);
+    }
+
+    .mobile-nav-leave-to {
+        transform: translateX(-250px);
+    }
+
 }
 </style>
