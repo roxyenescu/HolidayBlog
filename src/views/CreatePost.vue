e8fc54ae4c0bea660eca3bdefb6b43e457cf6b3c
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2024-02-04 17:33:54 +0200

    upload multiple images in CreatePost

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -8,7 +8,7 @@
       <div class="blog-info">
         <input type="text" placeholder="Enter Blog Title" v-model="blogTitle" />
         <div class="upload-file">
-          <label for="blog-photo">Upload cover Photo</label>
+          <label class="blog-photo-label" for="blog-photo">Upload Cover Photo</label>
           <input type="file" ref="blogPhoto" id="blog-photo" @change="fileChange" accept=".png, .jpg, .jpeg" />
           <button @click="openPreview" class="preview"
             :class="{ 'button-inactive': !this.$store.state.blogPhotoFileURL }">Preview
@@ -16,14 +16,47 @@
           <span>File Chosen: {{ this.$store.state.blogPhotoName }}</span>
         </div>
       </div>
-      <div class="editor">
-        <QuillEditor placeholder="Write your blog text here..." :editorOptions="editorSettings" toolbar="full"
-          useCustomImageHandler @image-added="imageHandler" type="text" v-model="blogHTML">
-          <input type="text" v-model="blogHTML" />
-        </QuillEditor>
+
+      <div class="editor-container">
+        <div class="editor-settings">
+
+          <label class="uploadLabel" for="blog-photo-in">
+            <img :src="uploadPhoto" class="icon" @click="openImageUploader" />
+          </label>
+
+          <img :src="bold" @click="applyBold" class="icon" />
+          <img :src="italic" @click="applyItalic" class="icon" />
+          <img :src="underline" @click="applyUnderline" class="icon" />
+        </div>
+
+        <div class="editor-frame">
+          <textarea id="blog-text" v-model="blogHTML" @added-image="imageHandler"></textarea>
+        </div>
+
+        <div class="card">
+          <div class="top">
+            <p>Drag & drop images</p>
+          </div>
+          <div class="drag-area" @dragover.prevent="onDragOver" @dragleave.prevent="onDragLeave" @drop.prevent="onDrop">
+            <span v-if="!isDragging">
+              Drag & drop image here or
+              <span class="select" role="button" @click="selectFiles">Choose</span>
+            </span>
+            <div v-else class="select">Drop images here</div>
+            <input name="file" type="file" class="file" ref="fileInput" multiple @change="onFileSelect" />
+          </div>
+          <div class="container-images">
+            <div class="image" v-for="(image, index) in images" :key="index">
+              <span class="delete" @click="deleteImage(index)">&times;</span>
+              <img :src="image.url" />
+            </div>
+          </div>
+          <button type="button">Upload</button>
+        </div>
       </div>
+
       <div class="blog-actions">
-        <button>Publish Blog</button>
+        <button @click="saveBlog">Publish Blog</button>
         <router-link class="router-button" :to="{ name: 'BlogPreview' }">Post Preview</router-link>
       </div>
     </div>
@@ -35,21 +68,23 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 import BlogCoverPreview from '../components/BlogCoverPreview.vue';
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
-import '@vueup/vue-quill/dist/vue-quill.bubble.css';
+import bold from '../assets/Icons/text-bold-svgrepo-com.svg';
+import italic from '../assets/Icons/text-italic-svgrepo-com.svg';
+import underline from '../assets/Icons/text-underline-svgrepo-com.svg';
+import uploadPhoto from '../assets/Icons/frame-4-svgrepo-com.svg';
 
-import { QuillEditor } from '@vueup/vue-quill';
-
-import Quill from 'quill'
-import { ImageDrop } from 'quill-image-drop-module'
-const ImageResize = require('quill-image-resize-module').default
-Quill.register('modules/imageDrop', ImageDrop)
-Quill.register('modules/imageResize', ImageResize)
 
 export default {
   name: "CreatePost",
+  setup() {
+    return {
+      bold,
+      italic,
+      underline,
+      uploadPhoto,
+    }
+  },
   components: {
-    QuillEditor,
     BlogCoverPreview,
   },
   data() {
@@ -57,15 +92,47 @@ export default {
       file: null,
       error: null,
       errorMsg: null,
-      editorSettings: {
-        modules: {
-          imageDrop: true,
-          imageResize: true,
-        },
-      },
+      images: [],
+      isDragging: false,
     };
   },
   methods: {
+    selectFiles() {
+      this.$refs.fileInput.click();
+    },
+    onFileSelect(event) {
+      const files = event.target.files;
+      if (files.length === 0) return;
+      for (let i = 0; i < files.length; i++) {
+        if (files[i].type.split("/")[0] != "image") continue;
+        if (!this.images.some((e) => e.name === files[i].name)) {
+          this.images.push({ name: files[i].name, url: URL.createObjectURL(files[i]) });
+        }
+      }
+    },
+    deleteImage(index) {
+      this.images.splice(index, 1);
+    },
+    onDragOver(event) {
+      event.preventDefault();
+      event.isDragging = true;
+      event.dataTransfer.dropEffect = "copy";
+    },
+    onDragLeave(event) {
+      event.preventDefault();
+      this.isDragging = false;
+    },
+    onDrop(event) {
+      event.preventDefault();
+      this.isDragging = false;
+      const files = event.dataTransfer.files;
+      for (let i = 0; i < files.length; i++) {
+        if (files[i].type.split("/")[0] != "image") continue;
+        if (!this.images.some((e) => e.name === files[i].name)) {
+          this.images.push({ name: files[i].name, url: URL.createObjectURL(files[i]) });
+        }
+      }
+    },
     fileChange() {
       this.file = this.$refs.blogPhoto.files[0];
       const fileName = this.file.name;
@@ -119,6 +186,7 @@ export default {
       },
       set(payload) {
         this.$store.commit("newBlogPost", payload);
+
       },
     },
   },
@@ -139,8 +207,8 @@ export default {
     color: #fff;
   }
 
-  label,
   button,
+  .blog-photo-label,
   .router-button {
     transition: 0.5s ease-in-out all;
     align-self: center;
@@ -230,26 +298,149 @@ export default {
     }
   }
 
-  .editor {
-    height: 60vh;
+  .editor-container {
+    position: relative;
     display: flex;
     flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
 
-    // .ql-editor.ql-blak {
-    //   position: relative;
-    //   display: flex;
-    //   flex-direction: column;
-    //   height: 100%;
-    // }
-    .ql-container {
+    .editor-settings {
+      flex: 1;
+      margin-left: 16px;
+      position: relative;
       display: flex;
-      flex-direction: column;
-      height: 100%;
-      overflow: scroll;
+
+      .icon {
+        width: 40px;
+        height: auto;
+        cursor: pointer;
+        font-size: 24px;
+        margin-left: 10px;
+      }
     }
 
-    .ql-editor {
-      padding: 20px 16px 30px;
+    .editor-frame {
+      height: 100%;
+      padding: 5px;
+      width: 100%;
+      margin-top: 16px;
+
+      textarea {
+        width: 100%;
+        height: 80%;
+        resize: none;
+        overflow-y: scroll;
+      }
+    }
+
+    .card {
+      width: 50%;
+      height: 50%;
+      padding: 10px;
+      box-shadow: 0 0 5px #fff;
+      border-radius: 5px;
+      overflow: hidden;
+      align-self: flex-start;
+
+      .top {
+        text-align: left;
+
+        p {
+          font-weight: bold;
+          color: red;
+        }
+      }
+
+      button {
+        outline: 0;
+        border: 0;
+        color: #fff;
+        border-radius: 4px;
+        font-weight: 400;
+        padding: 8px 13px;
+        width: 100%;
+        background: red;
+        width: 30%;
+      }
+
+      .drag-area {
+        height: 50%;
+        border-radius: 5px;
+        border: 2px dashed red;
+        background: #fff;
+        color: red;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        user-select: center;
+        -webkit-user-select: none;
+        margin-top: 10px;
+
+        .visible {
+          font-size: 18px;
+        }
+      }
+
+      .select {
+        color: #5256ad;
+        margin-left: 5px;
+        cursor: pointer;
+        transition: 0.4s;
+
+        &:hover {
+          opacity: 0.6;
+        }
+      }
+
+      .container-images {
+        width: 100%;
+        height: auto;
+        display: flex;
+        justify-content: flex-start;
+        align-items: flex-start;
+        flex-wrap: wrap;
+        max-height: 200px;
+        position: relative;
+        margin-bottom: 8px;
+        padding-top: 15px;
+
+
+        .image {
+          width: 75px;
+          margin-right: 5px;
+          height: 75px;
+          position: relative;
+          margin-bottom: 8px;
+
+          img {
+            width: 100%;
+            height: 100%;
+            border-radius: 5px;
+          }
+
+          span {
+            position: absolute;
+            top: -2px;
+            right: 9px;
+            font-size: 20px;
+            cursor: pointer;
+          }
+
+          .delete {
+            z-index: 999;
+            color: red;
+          }
+        }
+
+      }
+
+      input,
+      .drag-area .on-drop,
+      .drag-area.dragover .visible {
+        display: none;
+      }
     }
   }
 
